681a5e036c5ff271bb1b367c67b18bb0d0fbc981
Aakash Maharjan <aakash0977@gmail.com>
2024-03-18 20:55:00 +0545

    Added some style to card

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import { calculateTimeDifferenceInMinutes } from '../utils/commonUtils';
+import profilePic from '../img/profile.jpeg'
 
-const UserCard = ({user}) => {
+const UserCard = ({ user }) => {
     const lastOnline = Math.round(calculateTimeDifferenceInMinutes(user.time));
     const status = lastOnline < 10 ? 'Online' : 'Offline';
     return (
-            <div className={`card color${status}`} style={{ width: '18rem' }}>
-                <div className="card-body">
-                    <h5 className="card-title">{user.name}</h5>                    
-                    <h8 className="card-subtitle mb-2 text-body-secondary"><span>{lastOnline < 10 ? '🟢 ' : '🔴 '}</span>{lastOnline} min ago</h8>
-                    <p className="card-text">Lattitude: {user.lat}</p>
-                    <p className="card-text">Longitude: {user.lng}</p>
+        <div className='card'  style={{ width: '18rem' }}>
+            <div className="card-body">
+                <div className="user-info">
+                    <div className={`user-avatar${status}`} >
+                        <img src={profilePic} alt="User Avatar" className='avatar-image' />
+                    </div>
+                    <div className="user-details">
+                        <h5 className="card-title">{user.name}</h5>
+                        <h8 className="card-subtitle mb-2 text-body-secondary">
+                            <span>{lastOnline < 10 ? '🟢 online' : `🔴 ${lastOnline} min ago`}</span>
+                        </h8>
+                        <p className="card-text">Latitude: {user.lat}</p>
+                        <p className="card-text">Longitude: {user.lng}</p>
+                        <a href="#" className="card-link">History</a>
+                    </div>
                 </div>
             </div>
+        </div>
     );
 }
 
 export default UserCard;
-
