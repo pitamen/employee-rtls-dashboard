210b8b719a40a5ab93dfc8fded43e7378b80fad7
Pitambar Koirala <pitambar.koirala@dishhome.com.np>
2024-05-08 11:42:32 +0545

    hotfix text changed

--- src/components/Namebar.js
+++ src/components/Namebar.js
@@ -12,8 +12,8 @@ const Namebar = ({toggleFullScreen, isFullScreen=false}) => {
   
     return (
       <div style={namebarStyle} className="d-flex justify-content-between">
-        <button type="button" id='fullScreenButton' className="btn btn-outline btn-sm" >Dish home</button>
-        <h5 className='pt-1 ps-5'>Live Technician Locations</h5>
+        <button type="button" id='fullScreenButton' className="btn btn-outline btn-sm" >Dish Home</button>
+        <h5 className='pt-1 ps-5'>Live Locations</h5>
         <button type="button" id='fullScreenButton' className="btn btn-outline btn-sm" onClick={toggleFullScreen}>{isFullScreen?'Exit Full Screen': 'Full Screen'}</button>
       </div>
     );
