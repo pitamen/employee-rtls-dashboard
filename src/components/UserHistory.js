0a9dbce13479117d8ccc30b3a6246d0eab668aba
Aakash Maharjan <aakash0977@gmail.com>
2024-06-25 21:40:14 +0545

    temp

--- src/components/UserHistory.js
+++ src/components/UserHistory.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Namebar from "./Namebar";
 import Timepicker from "./Timepicker";
+import './SCSS/UserHistory.scss'
 
 const UserHistory = () => {
   // Example data array
@@ -13,21 +14,22 @@ const UserHistory = () => {
   return (
     <>
       <Namebar dashboardName="User History" />
-      <div className="container mx-auto px-4 py-8">
+      <div className="container mx-auto px-4 py-8 ">
         <div className="mb-6 pt-3">
           <Timepicker />
         </div>
-        <div className="mb-6">
+        <div className="mb-6 " >
           <div>
-            <div className="container py-2">
-              <div className="container py-2">
-                <table className="table table-striped">
+            <div className="container py-2  ">
+              <div className="container py-2 ">
+                <table className="table table-striped ">
                   <thead>
-                    <tr>
-                      <th scope="col">Date</th>
-                      <th scope="col">Username</th>
-                      <th scope="col">Tracked Date</th>
-                      <th scope="col">Tracked Location</th>
+                    <tr className="table-danger">
+                      <th scope="col"><b>User ID</b></th>
+                      <th scope="col"><b>Username</b></th>
+                      <th scope="col"><b>Tracked Time</b> </th>
+                      <th scope="col"><b>Tracked Location</b></th>
+                      <th scope="col"><b>Links</b></th>
                     </tr>
                   </thead>
                   <tbody>
@@ -36,6 +38,7 @@ const UserHistory = () => {
                         <th scope="row">{user.id}</th>
                         <td>{user.firstName}</td>
                         <td>{user.lastName}</td>
+                        <td>{user.handle}</td>
                         <td>{user.handle}</td>
                       </tr>
                     ))}
