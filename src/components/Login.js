a0b5ef23dee389a3e837a2aab800070de086851c
Aakash Maharjan <aakash0977@gmail.com>
2024-06-27 21:43:08 +0545

    change style of login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,59 +1,92 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { BASE_URL } from '../utils/constants';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import "./SCSS/Login.scss";
+import Logo from '../img/DishHome_Logo.svg.png'
 
 const Login = () => {
-
-  const [credentials, setcredentials] = useState({ username: "", password: "" })
+  const [credentials, setcredentials] = useState({
+    username: "",
+    password: "",
+  });
 
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await fetch(BASE_URL + "users/login", {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username: credentials.username, password: credentials.password })
+      body: JSON.stringify({
+        username: credentials.username,
+        password: credentials.password,
+      }),
     });
     const json = await response.json();
 
     if (json.success) {
-      sessionStorage.setItem('accessToken', json.accessToken);
-      navigate("/")
+      sessionStorage.setItem("accessToken", json.accessToken);
+      navigate("/");
+    } else {
+      alert("Invalid Credentials");
     }
-    else {
-      alert('Invalid Credentials')
-    }
-  }
+  };
   const onChange = (e) => {
-    setcredentials({ ...credentials, [e.target.name]: e.target.value })
-  }
+    setcredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
 
   return (
-    <div id='loginBg'>
+    <div id="loginBg">
       <div className='container d-flex align-items-center justify-content-center' id='loginWallpaper' style={{ height: '100vh' }}>
-        <form onSubmit={handleSubmit}>
-          <h2>RTLS Login</h2>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input type="text" className="form-control" id="username" name='username' value={credentials.username} onChange={onChange} required minLength={6} placeholder="Enter username" />
+      <div class="wrapper" >
+        <div class="logo">
+          <img
+            src={Logo}
+            alt=""
+          />
+        </div>
+        <div class="text-center mt-4 name">RTLS Login</div>
+        <form class="p-3 mt-3" onSubmit={handleSubmit}>
+          <div class="form-field d-flex align-items-center">
+            <span class="far fa-user"></span>
+            <input
+              type="text"
+              className="userName"
+              id="username"
+              name="username"
+              value={credentials.username}
+              onChange={onChange}
+              required
+              minLength={6}
+              placeholder="Enter username"
+            />
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" className="form-control" id="password" name='password' value={credentials.password} onChange={onChange} required minLength={8} placeholder="Password" />
+          <div class="form-field d-flex align-items-center">
+            <span class="fas fa-key"></span>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={credentials.password}
+              onChange={onChange}
+              required
+              minLength={8}
+              placeholder="Password"
+            />
           </div>
-          <div className="form-group form-check my-2">
-            <input type="checkbox" className="form-check-input" id="Remember me" />
-            <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
-          </div>
-          <div className='d-flex align-items-center '>
-            <button type="submit" className="btn btn-danger" >Login</button>
-            {/* <a className="btn btn-outline-secondary mx-2" href="/register" role='button'>Register</a> */}
-          </div>
+          <button type="submit" className="btn mt-3">
+            Login
+          </button>
         </form>
-      </div></div>
-  )
-}
+        <div class="text-center fs-6">
+          {/* <a href="#">Forget password?</a> or <a href="#">Sign up</a> */}
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+};
 export default Login;
---
+++ src/components/NewLogin.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../utils/constants';
+
+const Login = () => {
+
+  const [credentials, setcredentials] = useState({ username: "", password: "" })
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(BASE_URL + "users/login", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username: credentials.username, password: credentials.password })
+    });
+    const json = await response.json();
+
+    if (json.success) {
+      sessionStorage.setItem('accessToken', json.accessToken);
+      navigate("/")
+    }
+    else {
+      alert('Invalid Credentials')
+    }
+  }
+  const onChange = (e) => {
+    setcredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+
+  return (
+    <div id='loginBg'>
+      <div className='container d-flex align-items-center justify-content-center' id='loginWallpaper' style={{ height: '100vh' }}>
+        <form onSubmit={handleSubmit}>
+          <h2>RTLS Login</h2>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" className="form-control" id="username" name='username' value={credentials.username} onChange={onChange} required minLength={6} placeholder="Enter username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" className="form-control" id="password" name='password' value={credentials.password} onChange={onChange} required minLength={8} placeholder="Password" />
+          </div>
+          <div className="form-group form-check my-2">
+            <input type="checkbox" className="form-check-input" id="Remember me" />
+            <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
+          </div>
+          <div className='d-flex align-items-center '>
+            <button type="submit" className="btn btn-danger" >Login</button>
+            {/* <a className="btn btn-outline-secondary mx-2" href="/register" role='button'>Register</a> */}
+          </div>
+        </form>
+      </div></div>
+  )
+}
+export default Login;
